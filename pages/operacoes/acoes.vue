2e640e65c5941b6dbed7ffcecf617d057a6f25f0
Rodrigo Siqueira <rodrigo.siqueira@visie.com.br>
2021-03-08 23:36:36 -0300

    Primeiro layout copia operacoes

--- pages/operacoes/acoes.vue
+++ pages/operacoes/acoes.vue
@@ -1,3 +1,24 @@
 <template>
-  <p>Ações</p>
+  <b-container fluid>
+    <b-tabs pills card vertical>
+      <b-tab v-for="mes in meses" :key="mes" :title="mes" active>
+        <TablesOperacoes />
+      </b-tab>
+    </b-tabs>
+  </b-container>
 </template>
+<script>
+import moment from "moment";
+import "moment/locale/pt-br";
+import TablesOperacoes from "../../components/TablesOperacoes";
+export default {
+  components: {
+    TablesOperacoes,
+  },
+  data() {
+    return {
+      meses: moment.monthsShort(),
+    };
+  },
+};
+</script>
