249f3ee09ffdadb6d3133b32df3ac58a19458590
Rodrigo Siqueira <rodrigo.siqueira@visie.com.br>
2021-03-13 14:59:58 -0300

    Add files

---
+++ docs/_nuxt/9bc6f5a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{436:function(n,e,t){"use strict";t.r(e);var l=t(50),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("p",[n._v("Configurações")])}),[],!1,null,null,null);e.default=component.exports}}]);
